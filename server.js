e5be2a91e1281f6620eceea0d801141a9ed1669a
ChanBattilana <101348657+ChanBattilana@users.noreply.github.com>
2024-09-10 23:37:47 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-const path = require('path');
 const app = express();
 
-// Utilizar el puerto que Vercel nos asigna o el 3000 en local
+// Usa el puerto asignado por Vercel o 3000 para desarrollo local
 const port = process.env.PORT || 3000;
 
 // Habilitar CORS para permitir que el frontend haga solicitudes
@@ -11,6 +10,21 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
+});
+
+app.get('/cotizaciones', async (req, res) => {
+    try {
+        const response = await axios.get('https://dolarapi.com/v1/dolares');
+        const datos = response.data;  // Obtener los datos de la API
+        res.json(datos);  // Enviar los datos al frontend
+    } catch (error) {
+        console.error('Error al obtener los datos de la API:', error);
+        res.status(500).send('Error al obtener las cotizaciones.');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 // Servir archivos estáticos (HTML, CSS, JS)
