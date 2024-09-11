17b03f9173314770739fa7ed5fbb8901d9abbd19
ChanBattilana <101348657+ChanBattilana@users.noreply.github.com>
2024-09-11 14:33:41 -0300

    Add files via upload

---
+++ api/cotizaciones.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+    const url = 'https://dolarapi.com/v1/dolares'; // Cambia esta URL por la correcta
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Error en la solicitud');
+        }
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener los datos' });
+    }
+}
