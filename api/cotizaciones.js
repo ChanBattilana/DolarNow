f016f101dfdad23b9d87e39d489279d6b25ac1d7
ChanBattilana <101348657+ChanBattilana@users.noreply.github.com>
2024-09-11 14:35:00 -0300

    Update cotizaciones.js

--- api/cotizaciones.js
+++ api/cotizaciones.js
@@ -1,5 +1,7 @@
+// api/cotizaciones.js
+
 export default async function handler(req, res) {
-    const url = 'https://dolarapi.com/v1/dolares'; // Cambia esta URL por la correcta
+    const url = 'https://dolarapi.com/v1/dolares'; // Cambia esta URL si es necesario
     try {
         const response = await fetch(url);
         if (!response.ok) {
